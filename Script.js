069c6f00d11b1d4cbf61d519031194442835583f
Artem <artnovorai@gmail.com>
2016-12-05 19:14:53 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,8 +2,8 @@ var str1 = '# # # # #';
 var str2 = ' # # # # #';
 var i = 0;
     while( i < 3 ) {
-    console.log ( str1 )
-    console.log ( str2 )
-     i++;
+        console.log ( str1 )
+        console.log ( str2 )
+            i++;
     }
     console.log ( '# # # # #' )
