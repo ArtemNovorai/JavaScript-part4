d2c940aad8caabefa9d417765ff0d7acb00a890c
Artem <artnovorai@gmail.com>
2016-12-05 18:59:42 +0200

    JS4

---
+++ Script.js
@@ -0,0 +1,9 @@
+var str1 = '# # # # #';
+var str2 = ' # # # # #';
+var i = 0;
+    while( i < 3 ) {
+    console.log ( str1 )
+    console.log ( str2 )
+     i++;
+    }
+    console.log ( '# # # # #' )
